76d398c8c7b16af65ab6270e9c69179189fd89ba
Rio Prasetio <rioprst17@gmail.com>
2025-08-09 11:00:30 +0700

    first-commit

---
+++ front-end/pages/loginPage/script.js
@@ -0,0 +1,10 @@
+const submitButton = document.querySelector('.submit-button');
+const startButton = document.querySelector('.start-button');
+
+submitButton.addEventListener('click', () => {
+    window.location.href = '../homePage/index.html';
+});
+
+startButton.addEventListener('click', () => {
+    window.location.href = '../homePage/index.html';
+});
